7a00f56e9c3256ff1591586a0c146910f97f7b9e
Dimitri Demarkus <57415348+Gowixx@users.noreply.github.com>
2021-09-14 11:01:09 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,31 @@
+import os
+from colorama import Fore, init
+
+init()
+
+class info:
+    name = 'Indigo'
+    prefix = '[Indigo]'
+    version = 1.2
+    logSmallActions = False
+
+class logging:
+    def print(message: str) -> str:
+        return(
+            print(f'{Fore.LIGHTBLUE_EX}{info.prefix} {Fore.RESET}{message}')
+        )
+
+    def printError(message: str) -> str:
+        return(
+            print(f'{Fore.LIGHTBLUE_EX}{info.prefix} {Fore.RED}{message}')
+        )
+    
+    def verbose(message: str) -> str:
+        if logSmallActions:
+            return(print(message))
+        else:
+            return()
+    
+class utilities:
+    def cls():
+        return os.system('cls' if os.name == 'nt' else 'clear')
