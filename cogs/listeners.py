9e73ed75d9a5a8790b942e64eb653913183a9cff
Dimitri Demarkus <57415348+Gowixx@users.noreply.github.com>
2021-09-15 14:28:39 -0500

    v1.3 - Add Dank Memer Farmer, Nitro Sniper.

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -1,22 +1,65 @@
-import discord, base64, discum
+import discord, base64, discum, httpx, datetime, re, threading
 from discord.ext import commands
-from main import prefix, embedColor, accountToken
+from main import prefix, embedColor, accountToken, nitroSniper
 from utils import *
 
 class Listeners(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.checkedCodes = []
+
+    def snipeNitro(self, content: str):
+        if('discord.gift/' in content and nitroSniper):
+            startTime = datetime.datetime.now()
+            code = re.search(
+                pattern='discord.gift/(.*)',
+                string=content
+            ).group(1)
+            
+            if(code in self.checkedCodes):
+                return(logging.printError(f'Code {code} was already sent.'))
+            
+            headers = {
+                'user-agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; FSL 7.0.5.01003)',
+                'authorization': accountToken
+            }
     
+            request = httpx.post(
+                f'https://discordapp.com/api/v6/entitlements/gift-codes/{code}/redeem', 
+                headers=headers,
+            ).text
+        
+            elapsedTime = datetime.datetime.now() - startTime
+            elapsedTime = f'{elapsed.seconds}.{elapsed.microseconds}'
+
+            if 'subscription_plan' in request:
+                logging.print(f'| NITRO | Redeemed code {code}')
+
+            elif 'This gift has been redeemed already.' in request:
+                logging.printError(f'| NITRO | Code {code} already redeemed.')
+
+            elif 'Unknown Gift Code' in request:
+                logging.printError(f'| NITRO | {code} is Invalid!')
+            
+            print(f'{Fore.RESET}> {Fore.LIGHTCYAN_EX}Elapsed: {elapsedTime}')
+            return(self.checkedCodes.append(code))
+        
     @commands.command(name='togglesniper', description='Toggle one of the snipers.', usage='')
     async def togglesniper(self, ctx, sniper: str = 'nitro'):
         return await ctx.message.edit(content='> Coming soon.')
         # Code below is test
         sniper = sniper.lower()
         if sniper == 'nitro':
+            global nitroSniper
+            nitroSniper = False
             print('Disabled Nitro Sniper.')
         elif sniper == 'spam':
             print('Disabled Spam Sniper.')
-        
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        threading.Thread(target=self.snipeNitro, args=(message.content,)).start()
+
     @commands.Cog.listener()
     async def on_command(self, ctx):
         print(f'{Fore.LIGHTBLUE_EX}[Indigo] {Fore.RESET}Used command: {ctx.message.clean_content}')
