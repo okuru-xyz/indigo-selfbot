f3ed2e9a3407cc0fda6cb0a211ec05f9471dc5ba
Dimitri Demarkus <57415348+Gowixx@users.noreply.github.com>
2021-09-15 14:30:12 -0500

    v1.3 - Secretify, Base64Encode, Base64Decode

--- cogs/text.py
+++ cogs/text.py
@@ -1,4 +1,4 @@
-import discord
+import discord, base64
 from discord.ext import commands
 from main import prefix, embedColor
 
@@ -10,5 +10,37 @@ class Text(commands.Cog):
     async def purgehack(self, ctx):
         await ctx.message.edit(content='ﾠﾠ\n'* 400 + 'ﾠﾠ')
 
+    @commands.command(name='secretify', description='Add ||this|| to all characters in message.', usage='')
+    async def secretify(self, ctx, *, message: str):
+        out = ''
+        for letter in message:
+            out += f'||{letter}||'
+        await ctx.message.edit(content=out)
+
+    @commands.command(name='b64encode', description='Encode a base64 message.', usage=' [message]', aliases=['base64encode'])
+    async def b64encode(self, ctx, *, message: str):
+        try:
+            decoded = base64.b64encode(message.encode()).decode()
+        except:
+            decoded = '**Error**: Failed to encode.'
+        embed = discord.Embed(
+            title='Base64 Encode',
+            description=decoded,
+            color=embedColor
+        )
+        await ctx.message.edit(content='', embed=embed)
+
+    @commands.command(name='b64decode', description='Decode a base64 message.', usage=' [message]', aliases=['base64decode'])
+    async def b64decode(self, ctx, *, message: str):
+        try:
+            decoded = base64.b64decode(message.encode()).decode()
+        except:
+            decoded = '**Error**: Failed to decode.'
+        embed = discord.Embed(
+            title='Base64 Decode',
+            description=decoded,
+            color=embedColor
+        )
+        await ctx.message.edit(content='', embed=embed)
 def setup(client):
     client.add_cog(Text(client))
